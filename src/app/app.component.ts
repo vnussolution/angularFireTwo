fca905cb50062c6503fb6ab5d199dfc583746c5a
unknown <vnsusolution@gmail.com>
2017-09-26 09:23:03 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  constructor(private af: AngularFireDatabase) {
+    this.af.object(`testing`).subscribe(value => console.log('value:: ', value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MdButtonModule, Md, MdCheckboxModule } from '@angular/material';
+
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBHCGoAiRcNYOFzu-oaj3lC8vNRLgI7tM0",
+  authDomain: "angularfiretwo-d5b3a.firebaseapp.com",
+  databaseURL: "https://angularfiretwo-d5b3a.firebaseio.com",
+  projectId: "angularfiretwo-d5b3a",
+  storageBucket: "",
+  messagingSenderId: "700073074468"
+};
+
+////////////////setup material  ////////////////////////////
+@NgModule({
+  imports: [MdButtonModule, MdCheckboxModule],
+  exports: [MdButtonModule, MdCheckboxModule],
+})
+export class MyOwnCustomMaterialModule { }
+
+
+/////////////////////
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MyOwnCustomMaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
